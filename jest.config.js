bbd236e426f1981c5b6503fedfb0c4bd9d654fee
Sergey <sergey030901@mail.ru>
2024-08-19 14:45:36 +0300

    update config files

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   transform: {
-    '^.+\\.(ts|tsx)?$': 'ts-jest',
-    '^.+\\.(js|jsx)$': 'babel-jest',
+    '^.+\\.(ts)?$': 'ts-jest',
+    '^.+\\.(js)$': 'babel-jest',
   }
 };
